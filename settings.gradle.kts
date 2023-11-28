7f22b7682702a471831b82a01ff9188c5787cc67
respatica <respatica@hotmail.com>
2023-11-28 18:32:41 -0500

    i hate git

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CamiloPatino_SevastianSilva_COMP304Lab5Ex1"
+include(":app")
+ 
