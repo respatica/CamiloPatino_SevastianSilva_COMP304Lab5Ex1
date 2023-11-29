3dd30293dff0d9d381c7d681bd70f59e68411dbe
Sebastian Silva <sebastiansilvabenites@gmail.com>
2023-11-29 11:49:55 -0500

    Add other buildings

--- app/src/main/java/com/example/camilopatino_sevastiansilva_comp304lab5ex1/view/BuildingsActivity.kt
+++ app/src/main/java/com/example/camilopatino_sevastiansilva_comp304lab5ex1/view/BuildingsActivity.kt
@@ -2,8 +2,10 @@ package com.example.camilopatino_sevastiansilva_comp304lab5ex1.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.camilopatino_sevastiansilva_comp304lab5ex1.R
+import com.example.camilopatino_sevastiansilva_comp304lab5ex1.adapters.BuildingAdapter
 
 class BuildingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +13,30 @@ class BuildingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_buildings)
 
         val recyclerView = findViewById<RecyclerView>(R.id.buildings_list_view)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        var buildingsArray = arrayOf<String>()
 
+        var buildingType = intent.getStringExtra("type")
+
+        when(buildingType){
+            "Old Buildings"-> {
+                val oldBuildings = resources.getStringArray(R.array.old_buildings)
+                buildingsArray = oldBuildings
+            }
+            "Museums"-> {
+                buildingsArray = resources.getStringArray(R.array.museums)
+            }
+            "Stadiums" -> {
+                buildingsArray = resources.getStringArray(R.array.stadiums)
+            }
+            "Attractions"-> {
+                buildingsArray = resources.getStringArray(R.array.attractions)
+            }
+            else -> {}
+        }
+
+        val adapter = BuildingAdapter(buildingsArray)
+        recyclerView.adapter = adapter
 
     }
 }
