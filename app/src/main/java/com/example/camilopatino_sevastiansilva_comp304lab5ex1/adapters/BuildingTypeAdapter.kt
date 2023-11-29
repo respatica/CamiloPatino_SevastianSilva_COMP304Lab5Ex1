ce60259b612e43f2f9105fa2b48e483b88218be4
Sebastian Silva <sebastiansilvabenites@gmail.com>
2023-11-29 10:46:35 -0500

    Add Recycler View

---
+++ app/src/main/java/com/example/camilopatino_sevastiansilva_comp304lab5ex1/adapters/BuildingTypeAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.camilopatino_sevastiansilva_comp304lab5ex1.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.camilopatino_sevastiansilva_comp304lab5ex1.R
+
+class BuildingTypeAdapter(private var types: Array<String>) :  RecyclerView.Adapter<BuildingTypeAdapter.BuildingTypeViewHolder>(){
+
+    class BuildingTypeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView
+
+        init {
+            textView = view.findViewById(R.id.building_type_text)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BuildingTypeViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.building_type, parent, false)
+        return BuildingTypeViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return types.size
+    }
+
+    override fun onBindViewHolder(holder: BuildingTypeViewHolder, position: Int) {
+        holder.textView.text = types[position]
+    }
+
+}
--- app/src/main/java/com/example/camilopatino_sevastiansilva_comp304lab5ex1/view/MainActivity.kt
+++ app/src/main/java/com/example/camilopatino_sevastiansilva_comp304lab5ex1/view/MainActivity.kt
@@ -5,8 +5,11 @@ import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.camilopatino_sevastiansilva_comp304lab5ex1.databinding.ActivityMainBinding
 import com.example.camilopatino_sevastiansilva_comp304lab5ex1.R
+import com.example.camilopatino_sevastiansilva_comp304lab5ex1.adapters.BuildingTypeAdapter
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -25,6 +28,13 @@ class MainActivity : AppCompatActivity() , OnMapReadyCallback {
         val isMapsPermissionGranted = prefs.getBoolean("IS_MAP_PERMISSION_GRANTED", false)
         val isFineLocationPermissionGranted = prefs.getBoolean("IS_FINE_PERMISSION_GRANTED", false)
 
+        val recyclerView = findViewById<RecyclerView>(R.id.building_types_view)
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+
+        val types = resources.getStringArray(R.array.building_types)
+
+        val adapter = BuildingTypeAdapter(types)
+        recyclerView.adapter = adapter
 
 
         if (isMapsPermissionGranted) {
@@ -43,6 +53,9 @@ class MainActivity : AppCompatActivity() , OnMapReadyCallback {
                 )
             )
         }
+
+
+
     }
     private val locationPermissionRequest = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
